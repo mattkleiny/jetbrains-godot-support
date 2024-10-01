4f86f6166c526c8ad5fb2fe90da49ce4141d10e1
Matt Kleinschafer <xeusalmighty@gmail.com>
2024-10-01 12:25:06 +1000

    Tweak wrapper versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,8 @@ rootProject.name = "godot-support"
 
 pluginManagement {
     repositories {
-        maven { setUrl("https://cache-redirector.jetbrains.com/plugins.gradle.org") }
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
 
